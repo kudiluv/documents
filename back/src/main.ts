4bcec177a020ce68e4701cab434be5a4a6cf1bce
root <root@LAPTOP-90OCIJOB.localdomain>
2022-05-23 17:42:34 +0300

    feat: added ssl config

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const nestOptions: NestApplicationOptions = {};
+  if (process.env.SSH_KEY && process.env.SSH_CERT) {
+    nestOptions.httpsOptions = {
+      key: fs.readFileSync(process.env.SSH_KEY),
+      cert: fs.readFileSync(process.env.SSK_CERT),
+    };
+  }
+  const app = await NestFactory.create(AppModule, nestOptions);
+  nestOptions.cors = true;
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
